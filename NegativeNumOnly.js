be5e8d0342bff74cbc6fd621c692a39e23d60eb5
Theophilus Ndukwe <theondukwe@gmail.com>
2025-03-26 23:53:48 -0400

    Coding Challenge Completed

---
+++ NegativeNumOnly.js
@@ -0,0 +1,16 @@
+function makeNegative(num) {
+    //
+    if (num > 0){
+      
+      return -num
+    }
+    else if (num < 0){
+      
+      return num
+    }
+    else {
+      return 0
+    }
+      
+    
+  }
