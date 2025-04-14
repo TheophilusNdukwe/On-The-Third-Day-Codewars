0fad11c76bfa376686b50c2a6f9cea7ce55a0403
Theophilus <theondukwe@gmail.com>
2025-04-14 16:19:57 -0400

    coding challenge completed

---
+++ laliga.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  //3 leagues
+  //return the sum of goals in each league
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
