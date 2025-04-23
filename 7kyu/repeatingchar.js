49298ff871a68d880b218dfb0a0c593522b5dab2
Theophilus <theondukwe@gmail.com>
2025-04-23 09:16:38 -0400

    coding challenge completed

---
+++ 7kyu/repeatingchar.js
@@ -0,0 +1,27 @@
+// will I only be recieving a string containing letters?
+// so the string will contain numbers?
+// ignore lettercasing
+// assume empty string is an isogram
+//  returning true for letters that do not have repeating letters in them
+//  false for letters that do.
+function isIsogram(str){
+  let arr = []//create an empty array to store seen characters
+//   loop through each chracter in the string
+  for(let i = 0; i < str.length; i++){
+    let currentChar = str[i].toLowerCase() //convert to lowercase for case insensitivity
+//     Check if this character is already in our array
+    if(arr.includes(currentChar)){
+//       we found a repeat !
+      return false
+    }else{
+//       add this character to our arr
+      arr.push(currentChar)
+    }
+//     return true outside of the loop
+  }return true
+  
+}
+
+//Ex: "dermatoglyphics" --> true "no letters repeating"
+//Ex: "ada" --> false  "repeating"
+//Ex: "moOse" --> false  "o" repeating
